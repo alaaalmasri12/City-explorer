b493dba535b64c53456a81630e986d9e2f9154b7
alaaalmasri <alaaalmasri272@gmail.com>
2020-04-05 18:15:29 +0300

    lab 6

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-
+var ammanweatherdata=[];
 const server = express();
 
 server.use(cors());
@@ -20,11 +20,27 @@ server.listen(PORT, () => {
 server.get('/location', (req, res) => {
    
     const geoorphicalData =require('./data/geo.json');
-    const city = req.query.city;
+    const city = req.query.city_name;
     const locationData = new Location(city,geoorphicalData);
     res.send(locationData);
 
 })
+
+server.get('/weather', (req, res) => {
+   
+    const WeatherData =require('./data/weather.json');
+    const city = req.query.city;
+    let weatherarr=[];
+        for(let i=0;i<WeatherData.data.length;i++)
+    {
+        const weather = new Weather(city,WeatherData,i);
+weatherarr.push(weather);
+    }
+    res.send(weatherarr);
+
+})
+
+
 
 function Location (city,geoorphicalData) {
     this.search_query = city;
@@ -33,6 +49,17 @@ function Location (city,geoorphicalData) {
     this.longitude = geoorphicalData[0].lon;
 
 }
+function Weather(city,WeatherData,i)
+{
+    
+        this.forcast = WeatherData.data[i].weather.description;
+        this.time = WeatherData.data[i].datetime; 
+    
+        
+      
+    
+}
+
 
 
 server.use('*', (req, res) => {
