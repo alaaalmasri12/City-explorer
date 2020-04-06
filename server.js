d82ba60efdc6231979b807dad0581a70c6ac59e1
alaaalmasri12 <alaa_almasri1997@yahoo.com>
2020-04-07 00:43:59 +0300

    lab-07

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
+const superagent = require('superagent');
 
 const express = require('express');
 
 const cors = require('cors');
 
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-var ammanweatherdata=[];
 const server = express();
 
 server.use(cors());
@@ -18,44 +19,59 @@ server.listen(PORT, () => {
 
 
 server.get('/location', (req, res) => {
-   
-    const geoorphicalData =require('./data/geo.json');
-    const city = req.query.city_name;
-    const locationData = new Location(city,geoorphicalData);
+
+    const geoorphicalData = require('./data/geo.json');
+    const city = req.query.city;
+    const locationData = new Location(city, geoorphicalData);
     res.send(locationData);
 
 })
 
-server.get('/weather', (req, res) => {
-   
-    const WeatherData =require('./data/weather.json');
+server.get('/weather', weatherhandler)
+function weatherhandler(req, res) {
     const city = req.query.city;
-    let weatherarr=[];
-        for(let i=0;i<WeatherData.data.length;i++)
-    {
-        const weather = new Weather(city,WeatherData,i);
-weatherarr.push(weather);
-    }
-    res.send(weatherarr);
-
-})
-
-function Location (city,geoorphicalData) {
+    //     var weatherarr=[];
+    //    const WeatherData=require('./data/weather.json');
+    //         WeatherData.data.map((val)=>{
+    //             let weather = new Weather(city,val);
+    //              weatherarr.push(weather);
+    //     })
+    // res.send(weatherarr);
+    console.log(city);
+     getwather(city)
+        .then(weatherData => res.status(200).json(weatherData));
+}
+function Location(city, geoorphicalData) {
     this.search_query = city;
-    this.formatted_query =geoorphicalData[0].display_name;
+    this.formatted_query = geoorphicalData[0].display_name;
     this.latitude = geoorphicalData[0].lat;
     this.longitude = geoorphicalData[0].lon;
 }
-function Weather(city,WeatherData,i)
-{
-    
-        this.forcast = WeatherData.data[i].weather.description;
-        this.time = WeatherData.data[i].datetime; 
-    
+
+
+function getwather(city) {
+
+    let key = process.env.weatherAPI;
+    const watherinfo=[];    
+    const Url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+    console.log('asdasdasdsa',Url);
+    return superagent.get(Url)
+    .then(weatherData => {
+        weatherData.body.data.map(val => {
+          var weatherData = new Weather(val);
+          watherinfo.push(weatherData);
+          console.log(weatherData,city);
+        });
+        return watherinfo;
+
+        });
+
 }
+function Weather(day) {
 
-
-
+    this.forcast = day.weather.description;
+    this.time = day.datetime;
+}
 server.use('*', (req, res) => {
     res.status(404).send('NOT FOUND');
 });
